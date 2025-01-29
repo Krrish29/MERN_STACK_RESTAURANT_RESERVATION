bacbc92ff5e6f718ebe1ae7cc4e75665da06048f
krrish <krrishgarg4@gmail.com>
2025-01-29 18:02:11 +0530

    frontend

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -7,16 +7,16 @@ const Footer = () => {
         <div className="banner">
           <div className="left">ZEESHU</div>
           <div className="right">
-            <p>Z6 Gulshan-e-Maymar, Karachi</p>
+            <p> RDC,Ghaziabad </p>
             <p>Open: 05:00 PM - 12:00 AM</p>
           </div>
         </div>
         <div className="banner">
           <div className="left">
-            <p>Developed By CODEWITHZEESHU</p>
+            <p>Developed By KRRISH</p>
           </div>
           <div className="right">
-            <p>All Rights Reserved By CodeWithZeeshu.</p>
+            <p>All Rights Reserved By Krrish.</p>
           </div>
         </div>
       </div>
